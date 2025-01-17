5bd8b27f40a3ab8235b2236cee6a312e8f01633c
mihailgaberov <mihail.gaberov@gmail.com>
2025-01-17 09:31:36 +0100

    :student: draw a smile

--- script.js
+++ script.js
@@ -3,21 +3,35 @@ function draw() {
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
 
+    // Overlapping squares
     // ctx.fillStyle = "rgb(200 0 0)";
     // ctx.fillRect(10, 10, 50, 50);
 
     // ctx.fillStyle = "rgb(0 0 200 / 50%)";
     // ctx.fillRect(30, 30, 50, 50);
 
+    // Squares
     // ctx.fillRect(25, 25, 100, 100);
     // ctx.clearRect(45, 45, 60, 60);
     // ctx.strokeRect(50, 50, 50, 50);
 
+    // Arrow
+    // ctx.beginPath();
+    // ctx.moveTo(75, 50);
+    // ctx.lineTo(100, 75);
+    // ctx.lineTo(100, 25);
+    // ctx.fill();
+
+    // Smile
     ctx.beginPath();
-    ctx.moveTo(75, 50);
-    ctx.lineTo(100, 75);
-    ctx.lineTo(100, 25);
-    ctx.fill();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+    ctx.stroke();
   }
 }
 window.addEventListener("load", draw);
